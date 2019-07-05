e22654057a8a3e1d7ea6f786f4acb2406e676842
didacusabella <diequito93@gmail.com>
2019-07-05 12:04:12 +0200

    add first endpoint

--- public/index.js
+++ public/index.js
@@ -1,9 +1,18 @@
 import ko from "https://dev.jspm.io/knockout";
 
+const ENDPOINT_HOST = "localhost";
+const ENDPOINT_PORT = 3000;
+const FULL_ENDPOINT = `http://${ENDPOINT_HOST}:${ENDPOINT_PORT}`;
+
 class PocketViewModel {
   constructor(careers) {
     this.careers = ko.observableArray(careers);
   }
 }
 
-ko.applyBindings(new PocketViewModel(["Developer", "Project Manager", "Hacker"]));
+
+fetch(`${FULL_ENDPOINT}/career/`)
+.then(txt => txt.json())
+.then(response => response.map(element => element.name.value ))
+.then(mappedElements => ko.applyBindings(new PocketViewModel(mappedElements)))
+.catch(errors => console.error("Some Error Occurred:".concat(errors)));
